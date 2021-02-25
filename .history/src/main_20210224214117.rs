fed8583d8e7a22500cdf07d713409b1f11184e68
Harrison <harrisonhemstreet@gmail.com>
2021-02-24 21:52:38 -0700

    added x and y variables

---
+++ .history/src/main_20210224214117.rs
@@ -0,0 +1,19 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+ // actual size of the window
+ const SCREEN_WIDTH: i32 = 80;
+ const SCREEN_HEIGHT: i32 = 50;
+
+ const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+ struct Tcod {
+     root: Root,
+ }
+
+fn main() {
+   
+
+
+
+}
--- .history/src/main_20210224214842.rs
+++ .history/src/main_20210224214842.rs
@@ -12,9 +12,6 @@ use tcod::console::*;
  }
 
 fn main() {
-
-    tcod::system::set_fps(LIMIT_FPS);
-
     let root = Root::initializer()
     .font("arial10x10.png", FontLayout::Tcod)
     .font_type(FontType::Greyscale)
@@ -24,8 +21,7 @@ fn main() {
 
     let mut tcod = Tcod { root };
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    tcod::system::set_fps(LIMIT_FPS);
 
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
